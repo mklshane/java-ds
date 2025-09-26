b41e8afae11544dba282d78ba6c89726c63c7bea
Mikaela Shane Estrellanes <mikaela_shane_estrellanes@dlsl.edu.ph>
2025-09-26 23:51:02 +0800

    map

--- DataStruc/src/DSMap.java
+++ DataStruc/src/DSMap.java
@@ -1,3 +1,24 @@
+import java.util.*;
+
 public class DSMap {
+
+    public static List<List<String>> groupAnagrams(String[] strs){
+        Map<String, List<String>>  map = new HashMap<>();
+
+        for (String s: strs){
+            char[] arr = s.toCharArray();
+            Arrays.sort(arr);
+            String key = new String(arr);
+
+            map.computeIfAbsent(key, k -> new ArrayList<>()).add(s);
+        }
+        return new ArrayList<>(map.values());
+
+    }
+
+    public static void main(String[] args) {
+        String[] strs = {"eat","tea","tan","ate","nat","bat"};
+        System.out.println(groupAnagrams(strs));
+    }
 
 }
